29d5ce8cad211e4c7fd990bc64440a9fcbcea1ea
Rommyf1 <42357143+Rommyf1@users.noreply.github.com>
2024-04-12 09:07:30 -0400

    Change styles of "order movies by" component to get a better visual aspect

--- buscador-de-peliculas/src/App.jsx
+++ buscador-de-peliculas/src/App.jsx
@@ -63,6 +63,7 @@ function App() {
                 name="sortMoviesFilter"
                 value={sortMoviesFilter}
                 onChange={handleSortOption}
+                className="bg-gray-50 outline-none border border-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option value="none">No ordenar</option>
                 <option value="sortByName">Ordenar por Nombres</option>
