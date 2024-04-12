c4d7293e6f0840d38568eba896544cf786add996
Rommyf1 <42357143+Rommyf1@users.noreply.github.com>
2024-04-12 11:41:03 -0400

    added spacing and styles to elements in layout

--- cat-facts/src/App.jsx
+++ cat-facts/src/App.jsx
@@ -1,3 +1,4 @@
+
 import useCatFact from "./hooks/useCatFact";
 import useCatImage from "./hooks/useCatImage";
 
@@ -10,16 +11,16 @@ export default function App() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
+    <main className="min-h-screen flex flex-col items-center justify-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-sky-700">Aplicación de Gatitos</h1>
-      <article className="flex flex-col items-center">
+      <article className="flex flex-col items-center gap-4">
         <img
           src={catImage}
           alt={`Random Cat Image for the text ${catFact}`}
           className="w-96"
         />
-        <p>{catFact}</p>
-        <button onClick={handleClick}>Other Fact</button>
+        <p className="text-pretty">{catFact}</p>
+        <button onClick={handleClick} className="bg-sky-500 mt-4 py-2 px-2 outline-none rounded-md shadow-lg hover:bg-sky-700" >Other Fact</button>
       </article>
     </main>
   );
